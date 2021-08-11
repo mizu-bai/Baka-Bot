69cbe24f0cf7c2cbae1dd5f984a6fa81aea6009e
mizu-bai <shiragawa4519@outlook.com>
2021-08-11 21:58:53 +0800

    Update backend

--- backend/myapp.pl
+++ backend/myapp.pl
@@ -1,21 +1,10 @@
-#!/usr/bin/env perl
 use Mojolicious::Lite -signatures;
 
-get '/' => sub ($c) {
-  $c->render(template => 'index');
+websocket '/echo' => sub ($c) {
+  $c->on(json => sub ($c, $hash) {
+    $hash->{msg} = "echo: $hash->{msg}";
+    $c->send({json => $hash});
+  });
 };
 
 app->start;
-__DATA__
-
-@@ index.html.ep
-% layout 'default';
-% title 'Welcome';
-<h1>Welcome to the Mojolicious real-time web framework!</h1>
-
-@@ layouts/default.html.ep
-<!DOCTYPE html>
-<html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
-</html>
