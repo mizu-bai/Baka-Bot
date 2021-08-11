294487ad21e85048457fe50c5c96946d952173bc
mizu-bai <shiragawa4519@outlook.com>
2021-08-12 00:16:45 +0800

    Update Junkou Baka

--- backend/myapp.pl
+++ backend/myapp.pl
@@ -1,9 +1,42 @@
 use Mojolicious::Lite -signatures;
 
-websocket '/echo' => sub ($c) {
-  $c->on(json => sub ($c, $hash) {
-    $hash->{msg} = "echo: $hash->{msg}";
-    $c->send({json => $hash});
+<<EOF;
+
+api::ws
+  message template (json)
+  {
+    "type":    message type (number), 0: client side, 1: server side
+    "name":    user name (string)
+    "time":    unix time (number)
+    "content": message content (string)
+   }
+
+EOF
+
+websocket '/api/ws' => sub ($c) {
+  $c->on(json => sub ($c, $c_msg) {
+    # get client side message
+    my $s_content = $c_msg->{constant};
+
+    # modify client side message to generate server side message
+    if ($s_content =~ m/名字/g) {
+      $s_content = "我是人工智能 Bot（其实是人工智障）";
+    } else {
+      $s_content =~ s/你/我/g;
+      $s_content =~ s/吗//g;
+      $s_content =~ s/？/！/g;
+    }
+
+    # server side message json
+    my $s_msg = {
+        type    => 1,
+        name    => "人工智障 Bot",
+        time    => time(),
+        content => $s_content
+    };
+
+    # send
+    $c->send({json => $s_msg});
   });
 };
 
